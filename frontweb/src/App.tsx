6f91a2ab65f33ce42c6e4ecb06befd875ce8be19
Greicy Itakura <greicyitakura@gmail.com>
2022-10-05 18:49:01 -0300

    routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,14 +1,14 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
-import Catalog from 'pages/Catalog';
+
+import Routes from 'Routes';
 
 const App = () => {
   return (
     <>
-    <Catalog />
-  </>
+      <Routes />
+    </>
   );
-}
+};
 
 export default App;
