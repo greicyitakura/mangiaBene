31605744af3ed4a555eb919a358104b31efe0aa3
Greicy Itakura <greicyitakura@gmail.com>
2022-10-06 20:41:33 -0300

    shop cart

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -3,7 +3,6 @@ import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 
-
 type Props = {
     product: Product;
 }
--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,40 +1,53 @@
-import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import CartIcon from 'assets/images/cart.png';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 const ProductDetails = () => {
-    
-    return (
-      <div className="product-details-container">
-        <div className="based-card product-details-card">
-            <Link to="/products">
-            <div className="goback-container">
-                <ArrowIcon/>
-                <h2>Voltar</h2>
+  return (
+    <div className="product-details-container">
+      <div className="based-card product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>Voltar</h2>
+          </div>
+        </Link>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://github.com/greicyitakura/mangiaBene/blob/main/pitII/imagens/antepasto.png?raw=true"
+                alt="Nome do produto"
+              />
             </div>
-            </Link>
-            <div className="row">
-                <div className="col-xl-6">
-                    <div className="img-container">
-                        <img src="https://github.com/greicyitakura/mangiaBene/blob/main/pitII/imagens/antepasto.png?raw=true" alt="Nome do produto" />
-                    </div>
-                    <div className="name-price-container">
-                        <h1>Nome do produto</h1>
-                        <ProductPrice price={2345.67} />
-                    </div>
-                </div>
-                <div className="col-xl-6">
-                    <div className="description-container">
-                        <h2>Descrição do produto</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, cumque!</p>
-                    </div>
-                </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
+            
             </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                cumque!
+              </p>
+            </div>
+            <Link to="/">
+                <div className="name-price-container-cart">
+                  <img src={CartIcon} alt="" />
+                  <h2>Adicionar</h2>
+                </div>
+              </Link>
+          </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default ProductDetails;
