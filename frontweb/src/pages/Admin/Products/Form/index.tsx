07a5b108864c7d68eebef8219d572821780ceb12
Greicy Itakura <greicyitakura@gmail.com>
2022-10-31 17:44:10 -0300

    form buttons

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -6,20 +6,35 @@ const Form = () => {
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">Dados do produto</h1>
         <form action="">
-          <div className="row">
-            <div className="col-lg-6">
-            <input type="text" className='form-control base-input'/>
-            <input type="text" className='form-control base-input'/>
-            <input type="text" className='form-control base-input'/>
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
-            
             <div className="col-lg-6">
-              <textarea name="" rows={10} className='form-control base-input'></textarea>
+              <div>
+                <textarea
+                  name=""
+                  rows={10}
+                  className="form-control base-input h-auto"
+                />
+              </div>
             </div>
           </div>
-          <div>
-            <button className='btn btn-outline-danger'>Cancelar</button>
-            <button className='btn btn-success'>Salvar</button>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-buttons">
+              Cancelar
+            </button>
+            <button className="btn btn-success product-crud-buttons">
+              Salvar
+            </button>
           </div>
         </form>
       </div>
