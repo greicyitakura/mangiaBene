3a9593c54470ce228556336bbeb065c213f55e42
Greicy Itakura <greicyitakura@gmail.com>
2022-10-31 17:17:21 -0300

    product crud form

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,9 +1,30 @@
+import './styles.css';
 
 const Form = () => {
-    return (
-     <h1>Tela form</h1>
-    )
-  }
-  
-  export default Form;
-  
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">Dados do produto</h1>
+        <form action="">
+          <div className="row">
+            <div className="col-lg-6">
+            <input type="text" className='form-control base-input'/>
+            <input type="text" className='form-control base-input'/>
+            <input type="text" className='form-control base-input'/>
+            </div>
+            
+            <div className="col-lg-6">
+              <textarea name="" rows={10} className='form-control base-input'></textarea>
+            </div>
+          </div>
+          <div>
+            <button className='btn btn-outline-danger'>Cancelar</button>
+            <button className='btn btn-success'>Salvar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
