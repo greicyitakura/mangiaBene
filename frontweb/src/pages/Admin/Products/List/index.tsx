ea51f222da8ff688ffc9fbe3784d75066318e34e
Greicy Itakura <greicyitakura@gmail.com>
2022-11-11 17:20:19 -0300

    refactor callback

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import Pagination from 'components/Pagination';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import { SpringPage } from 'types/vendor/spring';
@@ -9,19 +9,28 @@ import ProductCrudCard from '../ProductCrudCard';
 
 import './styles.css';
 
+type ControlComponentsData = {
+  activePage: number;
+};
+
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
-  useEffect(() => {
-    getProducts(0);
-  }, []);
+  const handlePageChance = (pageNumber: number) => {
+    setControlComponentsData({ activePage: pageNumber });
+  };
 
-  const getProducts = (pageNumber: number) => {
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+    });
+
+  const getProducts = useCallback(() => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: pageNumber,
+        page: controlComponentsData.activePage,
         size: 3,
       },
     };
@@ -29,7 +38,11 @@ const List = () => {
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  };
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
 
   return (
     <>
@@ -45,17 +58,14 @@ const List = () => {
         <div className="row">
           {page?.content.map((product) => (
             <div key={product.id} className="col-sm-6 col-md-12">
-              <ProductCrudCard
-                product={product}
-                onDelete={() => getProducts(page.number)}
-              />
+              <ProductCrudCard product={product} onDelete={getProducts} />
             </div>
           ))}
         </div>
-        <Pagination 
-        pageCount={(page) ? page.totalPages : 0}
-        range={3}
-        onChange={getProducts}
+        <Pagination
+          pageCount={page ? page.totalPages : 0}
+          range={3}
+          onChange={handlePageChance}
         />
       </div>
     </>
